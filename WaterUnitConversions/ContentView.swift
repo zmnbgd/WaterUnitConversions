46c4a67aaff0447d1f4453b209808a65f3a321db
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-17 18:57:16 +0100

    Initial Commit

---
+++ WaterUnitConversions/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  WaterUnitConversions
+//
+//  Created by Marko Zivanovic on 17.12.24..
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ WaterUnitConversions/WaterUnitConversionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  WaterUnitConversionsApp.swift
+//  WaterUnitConversions
+//
+//  Created by Marko Zivanovic on 17.12.24..
+//
+
+import SwiftUI
+
+@main
+struct WaterUnitConversionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
