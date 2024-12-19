08ffc0e0d2a161d6ea02f732c764956c58b18700
Marko Živanović <zivanovic.marko@icloud.com>
2024-12-19 22:33:32 +0100

    Water Conversion

--- WaterUnitConversions/ContentView.swift
+++ WaterUnitConversions/ContentView.swift
@@ -9,16 +9,57 @@ import SwiftUI
 
 struct ContentView: View {
     
+    @State private var inputAmount = 0.0
+    @State private var inputUnit = "Liter"
+    @State private var outputUnit = "Milliliter"
+    
+    let units = ["Liter", "Deciliter", "Centiliter", "Milliliter"]
+    
+    var convertedAmount: Double {
+            let inputInLiters: Double
+            switch inputUnit {
+            case "Liter":
+                inputInLiters = inputAmount
+            case "Deciliter":
+                inputInLiters = inputAmount / 10
+            case "Centiliter":
+                inputInLiters = inputAmount / 100
+            case "Milliliter":
+                inputInLiters = inputAmount / 1000
+            default:
+                inputInLiters = 0
+            }
+            
+            switch outputUnit {
+            case "Liter":
+                return inputInLiters
+            case "Deciliter":
+                return inputInLiters * 10
+            case "Centiliter":
+                return inputInLiters * 100
+            case "Milliliter":
+                return inputInLiters * 1000
+            default:
+                return 0
+            }
+        }
+    
     var body: some View {
         
         NavigationStack {
             Form {
-                Section("Choose a water measurement unit") {
-                    
+                Section("Water input amount") {
+                    TextField("Enter amount", value: $inputAmount, format: .number)
+                        .keyboardType(.decimalPad)
                 }
                 
-                Section {
-                    
+                Section("Choose Input Unit") {
+                    Picker("Input Unit", selection: $inputUnit) {
+                        ForEach(units, id: \.self) {
+                           Text($0)
+                        }
+                    }
+                    .pickerStyle(.navigationLink)
                 }
                 
                 Section("Choose a water measurement unit") {
@@ -29,7 +70,7 @@ struct ContentView: View {
                     
                 }
             }
-            .navigationTitle("Water Unit Conversions")
+            .navigationTitle("Water Conversions")
         }
     }
 }
